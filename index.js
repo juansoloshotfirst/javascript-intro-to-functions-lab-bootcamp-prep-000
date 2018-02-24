6c12b1387a7af3d11b0a0603caa35f3193419bb3
Jon Marquez <jon.marquez9258@gmail.com>
2018-02-24 05:11:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+ console.log ('HELLO')
+}
+function logWhisper(string) {
+  console.log ('hello')
+}
+function sayHiToGrandma(string) {
+ return "I love you, too."
+ console.log ("I love you, Grandma")
+}
+function sayHiToGrandma(string) {
+var lowercase = 'hello'
+lowercase.toLowerCase () === lowercase
+return "I can't hear you!" 
+}
+function sayHiToGrandma(string) {
+var uppercase = 'HELLO'
+uppercase.toUpperCase () === uppercase
+return "YES INDEED!"
+}
