3b7f9d892981c45de74fe667c902b0607abe456e
Jon Marquez <jon.marquez9258@gmail.com>
2018-02-24 05:28:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
      return "I can't hear you!"
    } else if (string === string.toUppercase()){
      return "YES INDEED!"
-   } else if (string === "I love you, Grandma.")
+   } else if (string === "I love you, Grandma."){
    return "I love you, too."
    }
  }
