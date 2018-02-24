5060f927a2e44be0f551e9521480897457e7e119
Jon Marquez <jon.marquez9258@gmail.com>
2018-02-24 05:27:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ function logWhisper(string) {
   console.log ('hello')
 }
 	 
- function sayHiToGrandma(string) {
- var lowercase = "hello"	
- var uppercase = "HELLO"
- if (string === lowercase) {
+ function sayHiToGrandma(string){
+   if (string === string.toLowerCase()){
+     return "I can't hear you!"
+   } else if (string === string.toUppercase()){
+     return "YES INDEED!"
+   } else if (string === "I love you, Grandma.")
+   return "I love you, too."
+   }
+   }
  }
- 
+   
  
  	
